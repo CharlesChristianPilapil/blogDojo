58febdebbddabb4875675e269d81d19509e2c7bc
CharlesChristianPilapil <charleschristian.pilapil@gmail.com>
2024-01-29 07:30:03 +0800

    update

--- src/BlogList.jsx
+++ src/BlogList.jsx
@@ -1,21 +1,25 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom';
+import React from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
 
-const BlogList = ({blogs}) => {
-
-//   const blogs = props.blogs;
-//   const title = props.title;
+const BlogList = ({ blogs }) => {
+  const navigate = useNavigate();
 
   return (
     <div className='blog-list'>
       {!blogs.length ? (
-        <p></p>
+        <p>No blogs available.</p>
       ) : (
         <>
           <h2>All Blogs</h2>
           {blogs.map((blog) => (
             <div className="blog-preview" key={blog.id}>
-              <NavLink to={`/blogs/${blog.id}`}>
+              <NavLink
+                to={blog.author === 'admin' ? '/admin' : `/blogs/${blog.id}`}
+                onClick={() => {
+                  // Additional logic if needed before navigation
+                  // For example, tracking analytics, sending events, etc.
+                }}
+              >
                 <h2>{blog.title}</h2>
                 <p>Written by: {blog.author}</p>
               </NavLink>
@@ -25,6 +29,6 @@ const BlogList = ({blogs}) => {
       )}
     </div>
   );
-}
+};
 
-export default BlogList
+export default BlogList;
